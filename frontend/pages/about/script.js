04efa61aa0260daa08a52a2aa60abf62c4c39c71
Stan Krivtsoff <stan.krivtsov@gmail.com>
2023-10-09 23:22:12 +0200

    prestart configuration

--- frontend/pages/about/script.js
+++
@@ -1,8 +0,0 @@
-const btnBack = document.querySelector('.btn--back');
-
-function backToHome() {
-  console.log('back');
-  window.location.href = '/';
-}
-
-btnBack.addEventListener('click', backToHome);
