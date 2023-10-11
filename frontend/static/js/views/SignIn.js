d85a55604f639f4b6a58df2cd1c633e8b44faa4b
Stan Krivtsov <s.krivtsov@quant-cube.com>
2023-10-11 12:03:24 +0200

    added server to import html docs

--- frontend/static/js/views/SignIn.js
+++ frontend/static/js/views/SignIn.js
@@ -1,15 +1,19 @@
-import AbstractView from "./AbstractView.js";
+import AbstractView from './AbstractView.js';
 
 export default class extends AbstractView {
-    constructor(params) {
-        super(params);
-        this.postId = params.id;
-        this.setTitle("Sign In");
-    }
+  constructor(params) {
+    super(params);
+    this.setTitle('SignIn');
+  }
 
-    async getHtml() {
-        return `
-            <h1>Sign innnnnnnnnnnn</h1>
-        `;
+  async getHtml() {
+    const response = await fetch('/static/js/htmls/SignIn.html'); // Используйте правильный путь к файлу
+    
+    if (response.ok) {
+      const html = await response.text();
+      return html;
+    } else {
+      return 'Failed to load HTML content';
     }
+  }
 }
