92a11a3619545c7820d3ebcb4a76fbc3772007fc
Stan Krivtsoff <stan.krivtsov@gmail.com>
2023-10-15 00:49:03 +0200

    added script to router

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,65 +1,82 @@
-import Dashboard from "./views/Dashboard.js";
-import Posts from "./views/Posts.js";
-import PostView from "./views/PostView.js";
-import Settings from "./views/Settings.js";
-import SignIn from "./views/SignIn.js";
+import Dashboard from './views/Dashboard.js';
+import Posts from './views/Posts.js';
+import PostView from './views/PostView.js';
+import Settings from './views/Settings.js';
+import SignIn from './views/SignIn.js';
 
-const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+const pathToRegex = (path) =>
+  new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
 
-const getParams = match => {
-    const values = match.result.slice(1);
-    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+const getParams = (match) => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
 
-    return Object.fromEntries(keys.map((key, i) => {
-        return [key, values[i]];
-    }));
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    })
+  );
 };
 
-const navigateTo = url => {
-    history.pushState(null, null, url);
-    router();
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
 };
 
 const router = async () => {
-    const routes = [
-        { path: "/", view: Dashboard },
-        { path: "/posts", view: Posts },
-        { path: "/posts/:id", view: PostView },
-        { path: "/settings", view: Settings },
-        { path: "/sign-in", view: SignIn }
-    ];
+  const routes = [
+    { path: '/', view: Dashboard },
+    { path: '/posts', view: Posts },
+    { path: '/posts/:id', view: PostView },
+    { path: '/settings', view: Settings },
+    { path: '/sign-in', view: SignIn }
+  ];
 
-    // Test each route for potential match
-    const potentialMatches = routes.map(route => {
-        return {
-            route: route,
-            result: location.pathname.match(pathToRegex(route.path))
-        };
-    });
+  // Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      result: location.pathname.match(pathToRegex(route.path))
+    };
+  });
 
-    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
 
-    if (!match) {
-        match = {
-            route: routes[0],
-            result: [location.pathname]
-        };
-    }
+  if (!match) {
+    match = {
+      route: routes[0],
+      result: [location.pathname]
+    };
+  }
 
-    const view = new match.route.view(getParams(match));
+  const view = new match.route.view(getParams(match));
 
-    document.querySelector("#app").innerHTML = await view.getHtml();
+  document.querySelector('#app').innerHTML = await view.getHtml();
+
+  function changeLocation() {
+    console.log('XXX');
+    window.location.href = '/';
+  }
+
+  if (match.route.path === '/sign-in') {
+    const btnNew = document.querySelector('.btn--bk');
+    btnNew.addEventListener('click', changeLocation);
+  }
 };
-    
-window.addEventListener("popstate", router);
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("click", e => {
-        if (e.target.matches("[data-link]")) {
-            e.preventDefault();
-            navigateTo(e.target.href);
-        }
-    });
+window.addEventListener('popstate', router);
 
-    router();
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', (e) => {
+    if (e.target.matches('[data-link]')) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+
+  router();
 });
